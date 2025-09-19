3b143155308de275df42941c1e9e5108fed3739d
Vishoo <vverma971@gmail.com>
2025-09-19 13:03:14 +0530

    loading

--- src/questions/github.jsx
+++ src/questions/github.jsx
@@ -3,9 +3,12 @@ import { useState } from 'react'
 const github = () => {
     const [data, setData] = useState(null);
     const [value, setValue] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const searchUser = async () =>{
         try{
+            setLoading(true)
+            setData(null)
         const res = await fetch(`https://api.github.com/users/${value}`
             , {
                 method:'GET',
@@ -15,22 +18,28 @@ const github = () => {
             }
         );
 
-        const data = await res.json();
+        if(!res){
+            throw new Error("no response")
+        }
 
+        const data = await res.json();
         console.log(data);
         setData(data);
-
+        setLoading(false)
         }
         catch(err){
             alert(err.message)
         }
-
     }
   return (
     <div>
         <h1>{value}</h1>
         <input onChange={(e)=>setValue(e.target.value)} value={value} />
         <button onClick={searchUser}>Search</button>
+
+        {
+            loading && ( <h1>Loading...</h1>)
+        }
 
         { data && (
             <div style={{border:'1px solid black', width:'300px', margin:'auto', marginTop:'20px', display:'flex', flexDirection:'column', alignItems:'center'}}>
